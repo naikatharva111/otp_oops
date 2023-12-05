562a48e36ed80dc3f63314a823c2c7661393e965
Atharva Vinod Naik <naikatharva111@gmail.com>
2023-12-06 00:34:42 +0530

    version_with_use_of_UML

--- Desktop/SE OTP Assignment , PRN-2130331245051 , Name-Atharva Vinod Naik/otp1_oops.py
+++ Desktop/SE OTP Assignment , PRN-2130331245051 , Name-Atharva Vinod Naik/otp1_oops.py
@@ -5,6 +5,9 @@ import smtplib
 from twilio.rest import Client
 
 class OTPGenerator:
+    """
+    Class for generating OTPs.
+    """
     def generate_otp(self):
         """
         Generate a 6-digit OTP.
@@ -76,53 +79,58 @@ class UserInputValidator:
         return email_value if UserInputValidator.validate_email(email_value) else input("Enter a valid email")
 
 class OTPSender:
-    def __init__(self, account_sid, auth_token, twilio_number):
+    """
+    Class for sending OTPs.
+    """
+    def __init__(self, account_sid, auth_token, twilio_number, otp_generator, input_validator):
         self.client = Client(account_sid, auth_token)
         self.twilio_number = twilio_number
+        self.otp_generator = otp_generator
+        self.input_validator = input_validator
 
-    def send_otp_over_mobile(self, sent_phone_no, sent_otp):
+    def send_otp_over_mobile(self, sent_phone_no):
         """
         Send OTP to the provided mobile number using Twilio.
 
         Args:
             sent_phone_no (str): The recipient's mobile number.
-            sent_otp (str): The OTP to be sent.
         """
+        generated_otp = self.otp_generator.generate_otp()
         message = self.client.messages.create(
-            body=f"Your 6 digit OTP is {sent_otp}",
+            body=f"Your 6 digit OTP is {generated_otp}",
             from_=self.twilio_number,
             to=f'+91{sent_phone_no}',
         )
         print(message.body)
 
-    def send_otp_over_email(self, sent_email, sent_otp):
+    def send_otp_over_email(self, sent_email):
         """
         Send OTP to the provided email address using SMTP.
 
         Args:
             sent_email (str): The recipient's email address.
-            sent_otp (str): The OTP to be sent.
         """
+        generated_otp = self.otp_generator.generate_otp()
         server = smtplib.SMTP('smtp.gmail.com', 587)
         server.starttls()
         server.login('naikatharva111@gmail.com', 'jbctzddvhpwysdhf')
-        email_message = f'Your 6 digit OTP is {sent_otp}'
+        email_message = f'Your 6 digit OTP is {generated_otp}'
         server.sendmail('naikatharva111@gmail.com', sent_email, email_message)
         server.quit()
 
 # Example usage:
 
-# Initialize OTPGenerator
+# Initialize OTPGenerator and UserInputValidator
 otp_generator = OTPGenerator()
-
-# Initialize UserInputValidator
 input_validator = UserInputValidator()
 
-# Initialize OTPSender with Twilio credentials
+# Initialize OTPSender with Twilio credentials, OTPGenerator, and UserInputValidator
 otp_sender = OTPSender(
     account_sid='ACc90b01dc3b9e5727a207731cb591b9a7',
-    auth_token='01ff4b225bc6f12d54bb991add7e0275',
-    twilio_number='+15856394364'
+    auth_token='4e9aaa48cbdd9aac7dc4a1f7a6c09c99',
+    twilio_number='+15856394364',
+    otp_generator=otp_generator,
+    input_validator=input_validator
 )
 
 # Get and validate user input for mobile and email
@@ -130,6 +138,5 @@ user_phone_no = input_validator.get_valid_mobile()
 user_email = input_validator.get_valid_email()
 
 # Generate and send OTP
-generated_otp = otp_generator.generate_otp()
-otp_sender.send_otp_over_mobile(user_phone_no, generated_otp)
-otp_sender.send_otp_over_email(user_email, generated_otp)
+otp_sender.send_otp_over_mobile(user_phone_no)
+otp_sender.send_otp_over_email(user_email)
